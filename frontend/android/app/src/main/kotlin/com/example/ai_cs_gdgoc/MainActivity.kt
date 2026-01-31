4d1d59c0dfffb60f12af93abff0831e1c802c6b7
ayeah <187853299+ayashar@users.noreply.github.com>
2026-01-31 17:04:41 +0700

    Initial commit: Setup project structure and Flutter frontend

---
+++ frontend/android/app/src/main/kotlin/com/example/ai_cs_gdgoc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ai_cs_gdgoc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ frontend/lib/styles/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color googleBlue = Color.fromRGBO(66, 133, 244, 1);
+  static const Color googleYellow = Color.fromRGBO(244, 180, 0, 1);
+  static const Color googleRed = Color.fromRGBO(219, 68, 55, 1);
+  static const Color googleGreen = Color.fromRGBO(15, 157, 88, 1);
+  static const Color textColor = Color(0xFF4A4A4A);
+}
